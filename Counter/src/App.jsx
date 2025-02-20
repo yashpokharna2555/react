4f84d46ed4037f3451cb1c6d9e7fa4108df52eaf
Yash Pokharna <yashpokharna2512@gmail.com>
2025-02-19 23:04:24 -0800

    First commit

--- Counter/src/App.jsx
+++ Counter/src/App.jsx
@@ -1,16 +1,33 @@
+import React from "react";
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import './index.css';
+
 
 function App() {
   const [count, setCount] = useState(0)
+  const onInsertHandler = (count) => {
+    if(count <1){
+      alert('No negative')
+    }
+    setCount((prev) => prev-1);
+  }
+  const onInsert2Handler = (count) => {
+    setCount((prev) => prev+1);
+  }
 
   return (
     <>
-      <h1 class="text-3xl font-bold underline">
+      <h1 className="text-3xl font-bold underline">
         Hello world!
       </h1>
+
+      <div className="flex items-center justify-center">
+        <span onClick={() => onInsertHandler()}>◀️</span>
+        <div>Counter: {count}</div>
+        <span onClick={() => onInsert2Handler()}>▶️</span>
+      </div>
+
     </>
   )
 }
---
+++ Counter/tailwind.config.js
@@ -0,0 +1,10 @@
+export default {
+    content: [
+      "./index.html",
+      "./src/**/*.{js,ts,jsx,tsx}", // Ensure this covers all component files
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  };
