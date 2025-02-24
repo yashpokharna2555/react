0178a96f6d8ee985e01b90e3e14d1a60c17310ab
Yash Pokharna <yashpokharna2512@gmail.com>
2025-02-23 17:16:38 -0800

    23-feb

--- to-do/src/App.jsx
+++ to-do/src/App.jsx
@@ -4,11 +4,34 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [task, setTask] = useState([])
+  const [complete, setComplete] = useState(0);
+
+  const onAddHandler = (content) => {
+    setTask([...task,content]);
+  }
 
   return (
     <>
-      <div className="text-red-500 text-xl font-bold">Hello</div>
+      <div className="container">
+        <h1>TO-DO List</h1>
+
+
+        <div class="task-summary">
+          <p>Total Tasks: <span id="total-tasks">0</span></p>
+          <p>Completed: <span id="completed-tasks">0</span></p>
+        </div>
+
+
+        <div class="task">
+          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+          <input type="checkbox" />
+          <button>Delete Task</button>
+        </div>
+
+
+        <button class="add-task-btn" onClick={()=>onAddHandler}>Add Task</button>
+      </div>
 
     </>
   )
